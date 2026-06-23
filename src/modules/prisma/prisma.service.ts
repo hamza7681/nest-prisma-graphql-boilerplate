ec6dfe7228158d8a8b5aee29b98d51f3b88fe0eb
hamza7681 <hamzambf@gmail.com>
2026-06-23 18:19:11 +0500

    utils added to prisma service

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'src/generated/prisma/client';
+import { asyncTryCatch } from 'src/utils/tryCatch';
 
 @Injectable()
 export class PrismaService
@@ -15,10 +16,20 @@ export class PrismaService
   }
 
   async onModuleInit() {
-    await this.$connect();
+    const { error } = await asyncTryCatch({
+      fn: async () => await this.$connect(),
+    });
+
+    if (error) console.error('Failed to connect to the database:', error);
+    else console.log('Successfully connected to the database');
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    const { error } = await asyncTryCatch({
+      fn: async () => await this.$disconnect(),
+    });
+
+    if (error) console.error('Failed to disconnect from the database:', error);
+    else console.log('Successfully disconnected from the database');
   }
 }
